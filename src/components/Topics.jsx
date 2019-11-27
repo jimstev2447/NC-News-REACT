a0ddc1a7fb1941441e49aaebd0fd23b43e7dadb3
James Stevenson <jamesstevenson@laurens-MacBook-Air.local>
2019-11-27 12:35:16 +0000

    added err handling to topics

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -2,21 +2,34 @@ import TopicCard from './TopicCard';
 import React, { Component } from 'react';
 import * as api from '../uitls/utils';
 import Loader from './Loader';
+import ErrHandler from './ErrHandler';
 
 class Topics extends Component {
-  state = { topics: [], isLoading: true };
+  state = { topics: [], isLoading: true, err: '' };
 
   componentDidMount() {
-    api.getTopics().then(topics => {
-      this.setState({ topics, isLoading: false });
-    });
+    api
+      .getTopics()
+      .then(topics => {
+        this.setState({ topics, isLoading: false, err: '' });
+      })
+      .catch(
+        ({
+          response: {
+            status,
+            data: { msg }
+          }
+        }) => {
+          this.setState({ err: { msg, status }, isLoading: false });
+        }
+      );
   }
 
   render() {
-    const { isLoading, topics } = this.state;
-    return isLoading ? (
-      <Loader />
-    ) : (
+    const { isLoading, topics, err } = this.state;
+    if (isLoading) return <Loader />;
+    if (err) return <ErrHandler status={err.status} msg={err.msg} />;
+    return (
       <main>
         <h2>All topics</h2>
         {topics.map(topic => {
